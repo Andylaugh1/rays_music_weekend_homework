8107b13efad18f96fa0d422300cd5e541b11b475
Andy Laughlin <atl11384@gmail.com>
2018-05-26 11:00:02 +0100

    Test passing to count stock of instruments

--- src/main/java/Shop.java
+++ src/main/java/Shop.java
@@ -1,12 +1,22 @@
+import java.util.ArrayList;
+
 public class Shop {
 
     private String name;
+    private ArrayList<Instrument> instrumentStock;
+    private ArrayList<Accessory> accessoryStock;
 
     public Shop(String name){
         this.name = name;
+        this.instrumentStock = new ArrayList<>();
+        this.accessoryStock = new ArrayList<>();
     }
 
     public String getName() {
         return name;
+    }
+
+    public int countInstrumentStock() {
+        return instrumentStock.size();
     }
 }
--- src/test/java/ShopTest.java
+++ src/test/java/ShopTest.java
@@ -6,14 +6,25 @@ import static org.junit.Assert.assertEquals;
 public class ShopTest {
 
     Shop shop;
+    Instrument instrument;
+    GuitarString guitarString,guitarString2;
+    Fretboard fretboard;
 
     @Before
     public void before(){
         shop = new Shop("Ray's Music Store");
+        guitarString = new GuitarString("Fender", 10.00, 15.00);
+        fretboard = new Fretboard("Fender", 40.00, 60.00);
+        instrument = new Guitar("Fender", "Stratocaster", 200.00, 550.00, guitarString, fretboard);
     }
 
     @Test
     public void canGetName(){
         assertEquals("Ray's Music Store", shop.getName());
+    }
+
+    @Test
+    public void canCountInstrumentStock(){
+        assertEquals(0, shop.countInstrumentStock());
     }
 }
