3cfa0ae4da70ea9b76813142be21a16d79c17a63
Andy Laughlin <atl11384@gmail.com>
2018-05-26 10:31:40 +0100

    Test passing to get shop name

--- src/test/java/ShopTest.java
+++ src/test/java/ShopTest.java
@@ -1,2 +1,19 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class ShopTest {
+
+    Shop shop;
+
+    @Before
+    public void before(){
+        shop = new Shop("Ray's Music Store");
+    }
+
+    @Test
+    public void canGetName(){
+        assertEquals("Ray's Music Store", shop.getName());
+    }
 }
